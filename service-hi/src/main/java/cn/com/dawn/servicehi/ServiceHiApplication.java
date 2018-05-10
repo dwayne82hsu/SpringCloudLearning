41c29b34948b19659ba44cb7f91e449885fd4085
徐黎明 <dwayne82hsu@hotmail.com>
2018-05-10 13:42:18 +0800

    no message

--- service-hi/src/main/java/cn/com/dawn/servicehi/ServiceHiApplication.java
+++ service-hi/src/main/java/cn/com/dawn/servicehi/ServiceHiApplication.java
@@ -1,12 +1,17 @@
 package cn.com.dawn.servicehi;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -17,12 +22,33 @@ public class ServiceHiApplication {
 		SpringApplication.run(ServiceHiApplication.class, args);
 	}
 
-	@Value("${server.port}")
-	String port;
+	private static final Logger LOG = Logger.getLogger(ServiceHiApplication.class.getName());
 
-	@RequestMapping("/hi")
-	public String home(@RequestParam String name) {
-		return "hi " + name + ",i am from port:" + port;
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
 	}
 
+	@RequestMapping("/hi")
+	public String callHome() {
+		LOG.log(Level.INFO, "calling trace service-hi  ");
+		return restTemplate.getForObject("http://localhost:8989/miya", String.class);
+	}
+
+	@RequestMapping("/info")
+	public String info() {
+		LOG.log(Level.INFO, "calling trace service-hi ");
+
+		return "i'm service-hi";
+
+	}
+
+	@Bean
+	public AlwaysSampler defaultSampler() {
+		return new AlwaysSampler();
+	}
 }
